fb4d193e5df11794db88e65e732cc7bdd11c0e97
DevLuiz-eng <luiznotbook2@gmail.com>
2026-03-31 12:16:58 -0300

    refactor: use ErrorResponse in global exception handler

--- src/main/java/com/luizdev/inventory_system_remastered/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/luizdev/inventory_system_remastered/exceptions/handler/GlobalExceptionHandler.java
@@ -1,45 +1,67 @@
 package com.luizdev.inventory_system_remastered.exceptions.handler;
 
+import com.luizdev.inventory_system_remastered.dto.error.ErrorResponse;
 import com.luizdev.inventory_system_remastered.exceptions.userExceptions.EmailAlreadyExistsException;
 import com.luizdev.inventory_system_remastered.exceptions.userExceptions.UserAlreadyInactiveException;
 import com.luizdev.inventory_system_remastered.exceptions.userExceptions.UserNotFoundException;
+import com.luizdev.inventory_system_remastered.builder.ErrorResponseBuilder;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex){
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex, HttpServletRequest request) {
 
         log.warn("Usuário não encontrado: {}", ex.getMessage());
 
         return ResponseEntity.
-                status(HttpStatus.NOT_FOUND).
-                body(ex.getMessage());
+                status(HttpStatus.NOT_FOUND).body(ErrorResponseBuilder.buildError(
+                        HttpStatus.NOT_FOUND,
+                        ex.getMessage(),
+                        request.getRequestURI()));
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<String> handleEmailExists(EmailAlreadyExistsException ex) {
+    public ResponseEntity<ErrorResponse> handleEmailExists(EmailAlreadyExistsException ex, HttpServletRequest request) {
 
         log.warn("Tentativa de duplicar email já cadastrado: {}", ex.getMessage());
 
-        return ResponseEntity.
-                status(HttpStatus.BAD_REQUEST).
-                body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ErrorResponseBuilder.buildError(
+                        HttpStatus.BAD_REQUEST,
+                        ex.getMessage(),
+                        request.getRequestURI()
+                ));
     }
 
     @ExceptionHandler(UserAlreadyInactiveException.class)
-    public ResponseEntity<String> handleAlreadyInactive(UserAlreadyInactiveException ex) {
+    public ResponseEntity<ErrorResponse> handleAlreadyInactive(UserAlreadyInactiveException ex, HttpServletRequest request) {
 
         log.warn("Usuário já está inativo: {}", ex.getMessage());
 
-        return ResponseEntity.
-                status(HttpStatus.BAD_REQUEST).
-                body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseBuilder.buildError(
+                        HttpStatus.BAD_REQUEST,
+                        ex.getMessage(),
+                        request.getRequestURI()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericError(Exception ex, HttpServletRequest request) {
+
+        log.error("erro inesperado", ex);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                ErrorResponseBuilder.buildError(
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        "Erro interno de servidor",
+                        request.getRequestURI()));
     }
 }
