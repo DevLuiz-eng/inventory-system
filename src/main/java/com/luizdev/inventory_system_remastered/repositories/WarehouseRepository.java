756f4924484871da1fe1de6cb7ad8252fef5c26a
DevLuiz-eng <luiznotbook2@gmail.com>
2026-04-17 21:42:19 -0300

    feat: improved logic

--- src/main/java/com/luizdev/inventory_system_remastered/repositories/WarehouseRepository.java
+++ src/main/java/com/luizdev/inventory_system_remastered/repositories/WarehouseRepository.java
@@ -1,6 +1,8 @@
 package com.luizdev.inventory_system_remastered.repositories;
 
 import com.luizdev.inventory_system_remastered.entity.Warehouse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
 
     List<Warehouse> findByActiveTrueOrderByNameAsc();
+    Page<Warehouse> findByActiveTrueOrderByNameAsc(Pageable pageable);
 }
